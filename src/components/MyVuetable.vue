83f522e81d20dd3c5b42dfb7f97a7f790402ecf6
Rati Wannapanop <rati.wannapanop@gmail.com>
2017-01-17 21:53:10 +0700

    lesson-6

--- src/components/MyVuetable.vue
+++ src/components/MyVuetable.vue
@@ -8,6 +8,8 @@
 
 <script>
 import Vuetable from 'vuetable-2/src/components/Vuetable'
+import accounting from 'accounting'
+import moment from 'moment'
 
 export default {
   components: {
@@ -23,23 +25,45 @@ export default {
         {
           name: 'birthdate',
           titleClass: 'text-center',
-          dataClass: 'text-center'
+          dataClass: 'text-center',
+          callback: 'formatDate|DD-MM-YYYY'
         },
         {
           name: 'nickname',
+          callback: 'allcap'
         },
         {
           name: 'gender',
           titleClass: 'text-center',
-          dataClass: 'text-center'
+          dataClass: 'text-center',
+          callback: 'genderLabel'
         },
         {
           name: 'salary',
           titleClass: 'text-center',
-          dataClass: 'text-right'
+          dataClass: 'text-right',
+          callback: 'formatNumber'
         }
       ]
   	}
+  },
+  methods: {
+    allcap (value) {
+      return value.toUpperCase()
+    },
+    genderLabel (value) {
+      return value === 'M'
+        ? '<span class="label label-warning"><span class="glyphicon glyphicon-star"></span> Male</span>'
+        : '<span class="label label-info"><span class="glyphicon glyphicon-heart"></span> Female</span>'
+    },
+    formatNumber (value) {
+      return accounting.formatNumber(value, 2)
+    },
+    formatDate (value, fmt = 'D MMM YYYY') {
+      return (value == null)
+        ? ''
+        : moment(value, 'YYYY-MM-DD').format(fmt)
+    }
   }
 }
 </script>
